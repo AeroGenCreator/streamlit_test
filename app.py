1566ba3068c60f20218c502fe3651a499a226436
AeroGenCreator <gallo85floyd@gmail.com>
2025-07-15 18:36:49 -0600

    iteracion 1 app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+'''El metodo header permite mostrar un titulo
+en la pagina web'''
+st.header('Lanzar una moneda')
+'''Permite mostrar un mensaje en la pagina
+web'''
+st.write('Esta aplicación aún no es funcional. En construcción.')
